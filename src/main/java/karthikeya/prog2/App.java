83fa5f41b55052530c7db846bac2fbe782106903
Praneeth13-2004 <karthikeyarsp@gmail.com>
2025-06-01 12:31:24 +0530

    Initial commit

---
+++ src/main/java/karthikeya/prog2/App.java
@@ -0,0 +1,13 @@
+package karthikeya.prog2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! hi im gradle and maven" );
+    }
+}
